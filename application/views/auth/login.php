1dfd56476975a56ddd6c2e562050e2e297974296
hakimhakiki <32893488+hakimhakiki@users.noreply.github.com>
2020-03-14 11:06:35 +0700

    perbaikan form validation login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -18,17 +18,17 @@
                     <div class="text-center">
                         <h1 class="h4 text-gray-900 mb-4">Selamat Datang di Sistem Informasi Sundari!</h1>
                     </div>
-                    <form class="user" method="POST" action="auth/konfirmasi">
+                    <form class="user" method="POST" action="<?php echo base_url('Auth/konfirmasi'); ?>">
                         <div class="form-group">
                             <input type="text" class="form-control form-control-user" id="nama" name="nama_sundari" placeholder="Nama">
                             <div class="invalid-feedback">
-                                <?php echo form_error('nama');?>
+                                <?php echo form_error('nama_sundari');?>
                             </div>
                         </div>
                         <div class="form-group">
                             <input type="password" class="form-control form-control-user" id="password" name="password_sundari" placeholder="Password">
                             <div class="invalid-feedback">
-                                <?php echo form_error('password');?>
+                                <?php echo form_error('password_sundari');?>
                             </div>
                         </div>
                         <button type='submit' class="btn btn-primary btn-user btn-block">
